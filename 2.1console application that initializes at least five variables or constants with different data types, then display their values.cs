a4f016b03cd60791bb06ca64094683e0f18814e5
Zxcuvhh <102206331+Zxcuvhh@users.noreply.github.com>
2022-05-28 19:09:33 +0800

    Add files via upload

---
+++ 2.1console application that initializes at least five variables or constants with different data types, then display their values.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class DatatypeProgram
+{
+    public static void Main(string[] args)
+    {
+        int num = 650;
+        float rate = 13.8f;
+        decimal amount = 50.63M;
+        char code = 'C';
+        bool isValid = true;
+        string name = "Ezekiel";
+
+
+        Console.WriteLine(num);
+        Console.WriteLine(rate);
+        Console.WriteLine(amount);
+        Console.WriteLine(code);
+        Console.WriteLine(isValid);
+        Console.WriteLine(name);
+
+    }
+}
