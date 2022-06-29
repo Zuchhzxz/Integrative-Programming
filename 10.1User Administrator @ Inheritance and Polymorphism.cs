e97b84f5534724aeb229d6af83798769700101f9
Mr.Bots <102206331+Zxcuvhh@users.noreply.github.com>
2022-06-29 22:41:39 +0800

    Create 10.1User Administrator @ Inheritance and Polymorphism.cs

---
+++ 10.1User Administrator @ Inheritance and Polymorphism.cs
@@ -0,0 +1,79 @@
+using System;
+
+namespace UserNamespace
+{
+    class Pragram
+    {
+        static void Main(string[] args)
+        {
+            User use = new User("Bote", "Ezekiel");
+            Administrator add = new Administrator("Ezekiel", "020", "200");
+
+            Console.WriteLine("Enter ID : "); string ID = Convert.ToString(Console.ReadLine());
+            add.verifyLogin(ID, "320242");
+
+            Console.WriteLine("Enter new Password : "); string pass = Convert.ToString(Console.ReadLine());
+            add.updataPassword(pass);
+
+            Console.WriteLine("Enter new updated password : "); string updated = Convert.ToString(Console.ReadLine());
+            use.updatePassword(updated);
+
+            Console.WriteLine("Enter updated Name : "); string updatedName = Convert.ToString(Console.ReadLine());
+            add.updateAdminName(updatedName);
+
+
+        }
+    }
+    class User
+    {
+        protected string user_id; protected string user_password;
+        public User(string id, string pass)
+        {
+            this.user_id = id;
+            this.user_password = pass;
+        }
+        public void verifyLogin(string id, string pass)
+        {
+            bool confirm = string.Equals(this.user_password, this.user_id);
+
+            if (confirm.Equals(true))
+            {
+                Console.WriteLine("registered user : " + confirm);
+            }
+            else
+            {
+                Console.WriteLine("registered user : " + confirm);
+            }
+
+        }
+        public void updatePassword(string newPassword)
+        {
+            this.user_password = newPassword;
+            Console.WriteLine("This is your new password : " + this.user_password);
+        }
+    }
+
+    class Administrator : User
+    {
+
+        private string admin_name;
+        public Administrator(string name, string id, string pass) : base(id, pass)
+        {
+            this.user_password = pass;
+            this.user_id = id;
+            this.admin_name = name;
+
+            Console.WriteLine("The data of the data of the user  \nUsername : " + this.admin_name + "\nUserID : " + this.user_id + "\nPassword : " + this.user_password + "\n\n");
+        }
+        public void updataPassword(string newPassword)
+        {
+            this.user_password = newPassword;
+            Console.WriteLine("Updated password : " + this.user_password);
+        }
+        public void updateAdminName(string name)
+        {
+            this.admin_name = name;
+            Console.WriteLine("Updated Admin Name : " + this.admin_name);
+        }
+    }
+}
